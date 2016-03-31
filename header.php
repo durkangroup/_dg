7898861e79ed64ff36a3cedc0325f418279a810a
Paul Carbo <pcarbo@durkangroup.com>
2016-03-31 06:33:32 -0400

    expanded media query breakpoints

--- header.php
+++ header.php
@@ -8,10 +8,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui">
 <meta name="theme-color" content="#bbb">
 
-<meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/assets/browserconfig.xml">
-<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/img/tile-144x144.png">
-<meta name="msapplication-TileColor" content="#1e1e1e">
-
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico">
 <link rel="icon" sizes="192x192" href="<?php echo get_template_directory_uri(); ?>/assets/img/touch.png">
 <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/touch.png">
